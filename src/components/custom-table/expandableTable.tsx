689a7233f0c0671748cdf4c4f1ac15bdfb3b878a
giannischiout <giannis.chiout@gmail.com>
2024-11-11 18:49:49 +0200

    feat: start building complex data grid

--- src/components/custom-table/expandableTable.tsx
+++ src/components/custom-table/expandableTable.tsx
@@ -1,41 +1,26 @@
 import * as React from 'react';
 
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Table from '@mui/material/Table';
-import Stack from '@mui/material/Stack';
-import {alpha, Typography} from '@mui/material';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import TableBody from '@mui/material/TableBody';
-import TableHead from "@mui/material/TableHead";
+import {Box, Card, Table, TableRow, TableHead, TableCell, TableBody, Typography} from "@mui/material"
 
 import { Scrollbar } from '../scrollbar';
+import IconButton from "@mui/material/IconButton";
+import {Iconify} from "../iconify";
 
 const columns = [
   {
     id: 'country',
     label: 'Country',
-    width: '150px',
+    width: '100px'
   },
   {
     id: 'taskName',
     label: 'Task Name',
-    width: '150px',
   },
   {
     id: 'agent',
     label: 'Agent',
-    width: '150px',
   },
 ];
-
-interface Column {
-  label: string;
-  minWidth?: number;
-  align?: 'right';
-  format?: (value: number) => string;
-}
 
 export type CustomExpandableTableProps = {
   data: any;
@@ -47,103 +32,83 @@ export function CustomExpandableTable(
     data,
     table,
   }: CustomExpandableTableProps) {
-  const dateColumns = Array.from(
-    new Set(
-      data.flatMap((country: any) =>
-        country.tasks.flatMap((task: any) =>
-          task.totalHours ? Object.keys(task.totalHours) : []
-        )
-      )
-    )
-  );
 
-  const dynamicColumns = [
-    ...columns,
-    ...dateColumns.map((date) => ({
-      id: date,
-      label: date,
-      minWidth: 100,
-    }))
-  ];
+  const dateColumns = createDateColumns(data).map((date) => ({
+    id: date,
+    label: date,
+  }));
+
+  const allColumns = [...columns, ...dateColumns, { id: 'total', label: 'Total', width: '150px' }];
+
+
+  const calculateDateTotals = (tasks: any[], date: string) => {
+    return tasks.reduce((total, task) => {
+      const dateTotal = task.agents.reduce((sum: any, agent: any) => {
+        return sum + (agent.dates[date] || 0);
+      }, 0);
+      return total + dateTotal;
+    }, 0);
+  };
 
   return (
     <Card>
       <Box sx={{ overflowX: 'auto', position: 'relative' }}>
         <Scrollbar>
-          <Table size={table.dense ? 'small' : 'medium'} sx={{ minWidth: 960 }}>
+          <Table size={table.dense ? 'small' : 'medium'} sx={{ minWidth: 960, }}>
             <TableHead>
-              <TableRow>
-                {dynamicColumns.map((column: any) => (
-                  <TableCell key={column.id}>
-                    {column.label}
+              <TableRow sx={{position: 'relative'}}>
+                {allColumns.map((column: any, index) => (
+                  <TableCell
+                   sx={{
+                     position: column.id === "total" ? 'sticky': 'relative',
+                     color: column.id === "total" ? "primary.main" : "text.secondary",
+                     right: 0,
+                   }}
+                  >
+                    <Typography sx={{fontSize: '13px', fontWeight: 'medium',  textWrap: 'nowrap' }}>{column.label}</Typography>
                   </TableCell>
                 ))}
-                <TableCell>Total</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {data.map((countryItem: any, countryIndex: number) =>{
-                return (
-                  <React.Fragment key={countryIndex}>
-                     {/* ============= SHOW COUNTRY ROW ============ */}
-                    <TableRow sx={{backgroundColor: "grey.300"}}>
-                      <TableCell colSpan={dynamicColumns.length}>
-                        <Stack direction="row" alignItems="center" spacing={1}>
-                          <Typography variant="subtitle1">{countryItem.country}</Typography>
-                        </Stack>
-                      </TableCell>
-                      <TableCell />
-                    </TableRow>
-                    {countryItem.tasks.map((task: any, taskIndex: number) => {
-                      // Calculate total hours for each task
-                      const totalHours = Object.values(task.totalHours).reduce((a: number, b: number) => a + b, 0);
-                      return (
-                        <React.Fragment key={taskIndex}>
-                          <TableRow>
-                            <TableCell />
-                            <TableCell>{task.taskName}</TableCell>
-                            <TableCell />
-                            {dateColumns.map((date) => (
-                              <TableCell key={date}>
-                                {task.totalHours[date] ?? '-'}
-                              </TableCell>
-                            ))}
-                            <TableCell>
-                              <Stack
-                                alignItems="center"
-                                justifyContent="center"
-                                borderRadius={1}
-                                sx={{
-                                  backgroundColor: 'primary.main',
-                                  color: 'white',
-                                  width: '35px',
-                                  height: '35px'
-                              }}>
-                                {totalHours }
-                              </Stack>
-                            </TableCell>
-                          </TableRow>
-                          {task.agents.map((agent: any, agentIndex: number) => (
-                            <TableRow key={agentIndex}>
-                              <TableCell />
-                              <TableCell />
-                              <TableCell>{agent.firstName}</TableCell>
-                              {dateColumns.map((date) => (
-                                <TableCell key={date}>
-                                  {agent.dates[date] ?? '-'}
-                                </TableCell>
-                              ))}
-                              <TableCell>
-                                {agent.dates ? Object.values(agent.dates).reduce((a: number, b: number) => a + b, 0) : '-'}
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </React.Fragment>
-                      )
-                    })}
-                  </React.Fragment>
-                )
-              })}
+                 {
+                   data.map((item:any, index) => {
+                     return (
+                       <TableRow>
+                          <TableCell>{item.country}</TableCell>
+                          <TableCell sx={{flexDirection: 'row', display: 'flex', alignItems: 'center', minWidth: '140px'}}>
+                            <IconButton>
+                              <Iconify icon="quill:expand" />
+                            </IconButton>
+                            <Typography sx={{fontSize: '14px'}}>
+                              {`Tasks (${item.tasks.length})`}
+                            </Typography>
+                          </TableCell>
+                         <EmptyCell />
+                         {dateColumns.map((dateColumn) => (
+                           <TableCell key={dateColumn.id}>
+                             <Typography sx={{fontSize: '14px'}}>
+                               {calculateDateTotals(item.tasks, dateColumn.id) }
+                                <Box component="span" sx={{fontSize: '12px', color: 'text.secondary'}}> hrs</Box>
+                             </Typography>
+                           </TableCell>
+                         ))}
+                          <TableCell
+                            sx={{
+                              backgroundColor: 'white',
+                              position:  'sticky',
+                              right: 0,
+                            }}>
+                            <Typography
+                              sx={{fontSize: '14px', fontWeight: 'bold'}}>
+                              {calculateTotalCountySum(item.tasks)}
+                            </Typography>
+                          </TableCell>
+                       </TableRow>
+                     )
+                   })
+                 }
+
             </TableBody>
           </Table>
         </Scrollbar>
@@ -151,3 +116,40 @@ export function CustomExpandableTable(
     </Card>
   );
 }
+
+
+const EmptyCell = () => {
+  return (
+    <TableCell sx={{textAlign: 'center'}}>-</TableCell>
+  )
+}
+
+// Helpers
+// ========================================================
+const calculateTotalCountySum = (tasks: any[]) => {
+   let sum = 0;
+    tasks.forEach((task: any) => {
+      task.agents.forEach((agent: any) => {
+        console.log(agent);
+        Object.values(agent.dates).forEach((hours: number) => {
+          sum += hours;
+        })
+      });
+    })
+  return sum;
+}
+
+// ========================================================
+const createDateColumns = (data: any): string[] => {
+  const dateColumns: Set<string> = new Set();
+  data.forEach((item: any) => {
+    item.tasks.forEach((task: any) => {
+      task.agents.forEach((agent: any) => {
+        Object.keys(agent.dates).forEach((date: string) => {
+          dateColumns.add(date);
+        });
+      });
+    });
+  });
+  return Array.from(dateColumns);
+};
--- src/sections/my-reports/index.tsx
+++ src/sections/my-reports/index.tsx
@@ -1,13 +1,6 @@
-import dayjs from "dayjs";
 import {useState, useEffect} from "react";
 
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
 
-import {countries} from "../../_mock/countries";
-import {Iconify} from "../../components/iconify";
-import { useGetSummary} from "../../actions/summary";
 import {DashboardContent} from "../../layouts/dashboard";
 import {useTable} from "../../components/custom-table/use-table";
 import {CustomReusableTable} from "../../components/custom-table";
@@ -17,45 +10,12 @@ import type {TableColumn} from "../../components/custom-table/_types";
 import {summaryData} from "../../_mock/summary";
 import {CustomExpandableTable} from "../../components/custom-table/expandableTable";
 
-interface TableRow {
-  firstName: string;
-  lastName: string;
-  timesheetDate: string;
-  email: string;
-  department: string;
-  position: string;
-  company: string;
-  country: string;
-  leCode: string;
-  division: string;
-  task: string;
-  process: string;
-  subProcess: string;
-  hoursWorked: number;
-  taskCountry: string;
-  relation: string;
-  parentId: number;
-}
-
-
 
 
 export function MySummaryView() {
   const table = useTable();
   const summary = summaryData;
-  const [slicedData, setSlicedData] = useState<IReport[]>([])
 
-  const MAX_DAYS_LENGTH = Math.max(
-    ...summary.map((item) =>
-      Math.max(
-        ...item.tasks.map((task) =>
-         Object.keys(task.totalHours).length)
-
-      )
-    ))
-
-
-  console.log({MAX_DAYS_LENGTH})
 
 
 
